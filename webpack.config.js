56c8bcee0ebd3806df9b508e9c956d3fa835c5e6
Marcos Iglesias <golodhros@gmail.com>
2017-09-14 20:36:54 -0700

    Adding eslint, dev server overlay, editorconfig and more

--- webpack.config.js
+++ webpack.config.js
@@ -26,39 +26,58 @@ const commonConfig = {
         // makes the discovery automatic so you don't have to restart.
         // See https://github.com/facebookincubator/create-react-app/issues/186
         new WatchMissingNodeModulesPlugin(path.resolve('node_modules')),
-        new DashboardPlugin({port: process.env.PORT})
+        new DashboardPlugin({port: process.env.PORT}),
     ],
 };
 
 const productionConfig = () => commonConfig;
 
 const developmentConfig = () => {
-  const config = {
-    devServer: {
-      // Enable history API fallback so HTML5 History API based
-      // routing works. Good for complex setups.
-      historyApiFallback: true,
+    const config = {
+        devServer: {
+            // Enable history API fallback so HTML5 History API based
+            // routing works. Good for complex setups.
+            historyApiFallback: true,
 
-      // Display only errors to reduce the amount of output.
-      stats: 'errors-only',
+            // Display only errors to reduce the amount of output.
+            stats: 'errors-only',
 
-      // Parse host and port from env to allow customization.
-      //
-      // If you use Docker, Vagrant or Cloud9, set
-      // host: options.host || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices
-      // unlike default `localhost`.
-      host: process.env.HOST, // Defaults to `localhost`
-      port: process.env.PORT, // Defaults to 8080
-    }
-  };
+            // overlay: true is equivalent
+            overlay: {
+                errors: true,
+                warnings: true,
+            },
 
-  return Object.assign(
-    {},
-    commonConfig,
-    config
-  );
+            // Parse host and port from env to allow customization.
+            //
+            // If you use Docker, Vagrant or Cloud9, set
+            // host: options.host || '0.0.0.0';
+            //
+            // 0.0.0.0 is available to all network devices
+            // unlike default `localhost`.
+            host: process.env.HOST, // Defaults to `localhost`
+            port: process.env.PORT, // Defaults to 8080
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    // ensure that ESLint gets executed before anything else
+                    enforce: 'pre',
+                    loader: 'eslint-loader',
+                    options: {
+                        emitWarning: true,
+                    },
+                },
+            ],
+        }
+    };
+
+    return Object.assign(
+        {},
+        commonConfig,
+        config
+    );
 };
 
 
