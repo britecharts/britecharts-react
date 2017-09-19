72bcbe726adfb6016cb7ac8c879c5d426133a00f
Marcos Iglesias <miglesias@eventbrite.com>
2017-09-18 18:01:20 -0700

    Simple jest setup

--- src/charts/legend/legendComponent.js
+++ src/charts/legend/legendComponent.js
@@ -2,3 +2,5 @@ const component = 'legend';
 
 console.log(`hello ${component}`);
 
+
+export default component;
---
+++ src/charts/legend/legendComponent.test.js
@@ -0,0 +1,8 @@
+import legend from './legendComponent';
+
+test('loads stuff', () => {
+    let expected = 'legend';
+    let actual = legend;
+
+    expect(actual).toEqual(expected);
+});
