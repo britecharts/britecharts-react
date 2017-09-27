10ada6a6c880fb5aa494dba3943946875f20dbb5
Marcos Iglesias <golodhros@gmail.com>
2017-09-27 16:10:23 -0700

    start jest tests for stackedArea

--- src/charts/stackedArea/StackedAreaComponent.test.js
+++ src/charts/stackedArea/StackedAreaComponent.test.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import StackedAreaComponent from './StackedAreaComponent';
 import { stackedAreaData } from '../../helpers/testData';
+
+import stackedArea from './stackedAreaChart';
 
 describe('Stacked Area Chart', () => {
 
     describe('render', () => {
 
         it('should render the chart container', () => {
-            const wrapper = render(<StackedAreaComponent />);
+            const wrapper = shallow(<StackedAreaComponent />);
 
             let expected = 1;
             let actual = wrapper.find('.stacked-area-container').length;
@@ -17,13 +19,46 @@ describe('Stacked Area Chart', () => {
             expect(actual).toEqual(expected);
         });
 
-        it('should render a basic chart with data', () => {
-            const wrapper = mount(<StackedAreaComponent data={stackedAreaData.with3Sources()} />);
+        describe('when data passed in', () => {
+            let createSpy;
 
-            let expected = 1;
-            let actual = wrapper.find('.stacked-area').length;
+            beforeEach(() => {
+                createSpy = jest.spyOn(stackedArea, 'create');
+            });
 
-            expect(actual).toEqual(expected);
+            afterEach(() => {
+                createSpy.mockReset();
+                createSpy.mockRestore();
+            });
+
+            it('should call the create method or the chart', () => {
+                mount(<StackedAreaComponent chart={stackedArea} data={stackedAreaData.with3Sources()} />);
+
+                let expected = 1;
+                let actual = createSpy.mock.calls.length;
+
+                expect(actual).toEqual(expected);
+            });
+
+            it('should call the create method or the chart with the container as the first argument', () => {
+                const wrapper = mount(<StackedAreaComponent chart={stackedArea} data={stackedAreaData.with3Sources()} />);
+
+                let expected = wrapper.node._rootNode;
+                let actual = createSpy.mock.calls[0][0];
+
+                expect(actual).toEqual(expected);
+            });
+
+            it('should call the create method or the chart with the configuration object as the second argument', () => {
+                const dataSet = stackedAreaData.with3Sources();
+
+                mount(<StackedAreaComponent chart={stackedArea} data={dataSet} />);
+
+                let expectedData = dataSet;
+                let actualData = createSpy.mock.calls[0][1].data;
+
+                expect(actualData).toEqual(expectedData);
+            });
         });
     });
 });
