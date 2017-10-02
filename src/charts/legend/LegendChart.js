59967e9d401fbdb4fd374ed7907834650f688d5b
Jaylum Chen <jaylum@eventbrite.com>
2017-10-02 11:36:58 -0700

    feedback, fix props for legend

--- src/charts/legend/LegendChart.js
+++ src/charts/legend/LegendChart.js
@@ -1,4 +1,48 @@
-import wrapChart from '../common/helpers';
 import legendChart from 'britecharts/dist/umd/legend.min';
+import {select} from 'd3-selection';
+import {validateConfiguration, validateContainer, validateData} from '../helpers/validation';
+import {applyConfiguration} from '../helpers/configuration';
 
-export default wrapChart(legendChart);
+const legend = {};
+let chart;
+
+legend.create = (el, data, configuration = {}) => {
+    let container = select(el);
+
+    chart = legendChart();
+
+    validateContainer(container);
+    validateData(data);
+    validateConfiguration(chart, configuration);
+
+    let chartConfigured = applyConfiguration(chart, configuration);
+
+    // Calls the chart with the container and dataset
+    container.datum(data).call(chartConfigured);
+
+    return chartConfigured;
+};
+
+legend.update = (el, data, configuration = {}) => {
+    let container = select(el);
+
+    validateContainer(container);
+    validateConfiguration(chart, configuration);
+
+    let chartConfigured = applyConfiguration(chart, configuration);
+
+    // Calls the chart with the container and dataset
+    if (data && data.length) {
+        container.datum(data).call(chartConfigured);
+    } else {
+        container.call(chartConfigured);
+    }
+
+    return chartConfigured;
+};
+
+legend.destroy = () => {
+    // Cleanup methods here
+};
+
+export default legend;
