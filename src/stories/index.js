7ea8c551d699b9b029e45a1675e3f0945b11fce4
Marcos Iglesias <golodhros@gmail.com>
2017-10-03 11:04:32 -0700

    Updating docs (#15)

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-// Demo Stuff
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-import { Button, Welcome } from '@storybook/react/demo';
-
 // Real thing
 import { stackedAreaData, legendData } from '../helpers/testData';
 import StackedAreaComponent from '../charts/stackedArea/StackedAreaComponent';
@@ -14,14 +9,6 @@ import LegendComponent from '../charts/legend/LegendComponent';
 
 // from NPM modules
 import 'britecharts/dist/css/britecharts.css';
-
-storiesOf('Welcome', module)
-    .add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-    .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-    .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>)
-    .add('with some test stuff', () => <Button>Testing baby</Button>);
 
 storiesOf('StackedArea', module)
     .add('with 3 sources data', () => <StackedAreaComponent data={stackedAreaData.with3Sources()} />)
