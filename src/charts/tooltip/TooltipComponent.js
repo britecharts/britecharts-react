25e2fb46d79073e691899fc7b71aaf2bb1264b92
Marcos Iglesias <golodhros@gmail.com>
2017-10-04 15:36:17 -0700

    Simple tooltip component (#16)
    * Simple tooltip component
    
    * Updating docs
    
    * Cleaning stuff
    
    * Renaming component

---
+++ src/charts/tooltip/TooltipComponent.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import tooltip from './tooltipChart';
+
+
+export default class TooltipComponent extends React.Component {
+
+    static PropTypes = {
+        dateFormat: PropTypes.string,
+        dateLabel: PropTypes.string,
+        locale: PropTypes.string,
+        nameLabel: PropTypes.string,
+        shouldShowDateInTitle: PropTypes.bool,
+        title: PropTypes.string,
+        topicLabel: PropTypes.string,
+        valueFormat: PropTypes.string,
+        valueLabel: PropTypes.string,
+        topicsOrder: PropTypes.arrayOf(PropTypes.string),
+
+        // hide
+        // show
+        // update(dataPoint, colorMapping, position)
+    }
+
+    static defaultProps = {
+        chart: tooltip,
+    }
+
+    componentDidMount() {
+        this.props.chart.create(this._rootNode, this._getChartConfiguration());
+    }
+
+    componentDidUpdate() {
+        this.props.chart.update(this._rootNode, this._getChartConfiguration());
+    }
+
+    componentWillUnmount() {
+        this.props.chart.destroy(this._rootNode);
+    }
+
+    /**
+     * We want to remove the chart and data from the props in order to have a configuration object
+     * @return {Object} Configuration object for the chart
+     */
+    _getChartConfiguration() {
+        let configuration = {...this.props};
+
+        delete configuration.data;
+        delete configuration.chart;
+
+        return configuration;
+    }
+
+    _setRef(componentNode) {
+        if (componentNode) {
+            this._rootNode = componentNode;
+        }
+    }
+
+    render() {
+        return (
+            <div className="tooltip-container" ref={this._setRef.bind(this)} />
+        );
+    }
+
+}
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-// Real thing
 import { stackedAreaData, legendData } from '../helpers/testData';
 import StackedAreaComponent from '../charts/stackedArea/StackedAreaComponent';
 import LegendComponent from '../charts/legend/LegendComponent';
+import TooltipComponent from '../charts/tooltip/TooltipComponent';
 
 // from NPM modules
 import 'britecharts/dist/css/britecharts.css';
@@ -13,6 +13,10 @@ import 'britecharts/dist/css/britecharts.css';
 storiesOf('StackedArea', module)
     .add('with 3 sources data', () => <StackedAreaComponent data={stackedAreaData.with3Sources()} />)
     .add('with 500px width and 200px height', () => <StackedAreaComponent data={stackedAreaData.with3Sources()} width={500} height={200} />);
+
+storiesOf('Tooltip', module)
+    .add('with 3 sources data', () => <TooltipComponent data={stackedAreaData.with3Sources()} />)
+    .add('with 2 sources data', () => <TooltipComponent data={stackedAreaData.with2Sources()} title="Test Title" />);
 
 storiesOf('Legend', module)
     .add('with 500x500 container', () => (
