05e5a0f6e0261d63f7c819fbddcc389f1920c80c
rubenmoya <rmoyarodriguez@gmail.com>
2017-12-08 13:06:17 +0100

    Extract donutChart fixtures from testData

--- src/helpers/testData.js
+++ src/helpers/testData.js
@@ -310,37 +310,6 @@ export const stackedAreaData = {
     with2Sources,
 };
 
-const with4Slices = () => [
-    {
-        quantity: 60,
-        percentage: 60,
-        name: 'React',
-        id: 1,
-    },
-    {
-        quantity: 20,
-        percentage: 20,
-        name: 'Ember',
-        id: 2,
-    },
-    {
-        quantity: 10,
-        percentage: 10,
-        name: 'Angular',
-        id: 3,
-    },
-    {
-        quantity: 10,
-        percentage: 10,
-        name: 'Backbone',
-        id: 4,
-    },
-];
-
-export const donutData = {
-    with4Slices,
-};
-
 const with6Points = () => [
     {
         name: 'Shiny',
