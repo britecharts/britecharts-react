e537967429a0d2101cf7aae7f90fdac9b5eadf45
Amber Rockwood <arockwood@eventbrite.com>
2018-01-10 16:50:12 -0800

    Switches  ->  in  (leftover change from pull request #95)

--- src/charts/loading/LoadingContainer.js
+++ src/charts/loading/LoadingContainer.js
@@ -33,7 +33,7 @@ export default class LoadingContainer extends PureComponent {
         loading: true,
     }
 
-    componentWillMount() {
+    componentDidMount() {
         if (this.props.data !== null) {
             this.setState(toggleLoading);
         }
