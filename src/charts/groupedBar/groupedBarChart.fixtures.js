b820b5e7c0e316a323678e49ff20ed9247dc6d48
speckospock <17017460+speckospock@users.noreply.github.com>
2018-02-05 15:58:03 -0800

    Implements groupedBarChart.js and groupedBarChart.test.js, builds out GroupedBar.test.js

---
+++ src/charts/groupedBar/groupedBarChart.fixtures.js
@@ -0,0 +1,35 @@
+const with3Groups = () => [
+    {
+        name: 'Alice',
+        group: 'Group A',
+        value: 0,
+    },
+    {
+        name: 'Betsy',
+        group: 'Group B',
+        value: 6,
+    },
+    {
+        name: 'Charlotte',
+        group: 'Group C',
+        value: 4,
+    },
+];
+
+const with2Groups = () => [
+    {
+        name: 'Ari',
+        group: 'Group A',
+        value: 12,
+    },
+    {
+        name: 'Beth',
+        group: 'Group B',
+        value: 15,
+    },
+];
+
+export default {
+    with3Groups,
+    with2Groups,
+};
