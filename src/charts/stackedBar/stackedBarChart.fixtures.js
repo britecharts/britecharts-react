a6a568a2764b708de6e87e3f0323b2011c86181c
speckospock <17017460+speckospock@users.noreply.github.com>
2018-02-05 20:11:10 -0800

    Implements stackedBarChart.js and stackedBarChart.test.js

---
+++ src/charts/stackedBar/stackedBarChart.fixtures.js
@@ -0,0 +1,35 @@
+const with3Sources = () => [
+    {
+        name: '2011-01',
+        stack: 'Direct',
+        value: 0,
+    },
+    {
+        name: '2011-02',
+        stack: 'Direct',
+        value: 3,
+    },
+    {
+        name: '2011-03',
+        stack: 'Direct',
+        value: 7,
+    },
+];
+
+const with2Sources = () => [
+    {
+        name: '2011-01',
+        stack: 'Direct',
+        value: 0,
+    },
+    {
+        name: '2011-02',
+        stack: 'Direct',
+        value: 3,
+    },
+];
+
+export default {
+    with3Sources,
+    with2Sources,
+};
