fc92025b09b6dd804fb5f102040175b5b791dc69
speckospock <17017460+speckospock@users.noreply.github.com>
2018-02-07 11:17:41 -0800

    Creates a todo for the loading state import in componentChart.js

--- src/charts/template/componentChart.js
+++ src/charts/template/componentChart.js
@@ -3,6 +3,7 @@ import {select} from 'd3-selection';
 import {validateConfiguration, validateContainer, validateData} from '../helpers/validation';
 import {applyConfiguration} from '../helpers/configuration';
 
+//TODO: Implement the correct loading state(line, bar, and donut are the available options atm)
 import { line as {{camelCase componentName}}LoadingState } from 'britecharts/dist/umd/loading.min';
 
 const {{camelCase componentName}} = {};
