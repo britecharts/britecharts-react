50733e2fe181a0626dec70181ff33c4c2dbf64fb
speckospock <17017460+speckospock@users.noreply.github.com>
2018-02-08 09:45:32 -0800

    [Feature] Auto-generated component boilerplate with Plop (#112)
    * Adds Plop configuration and templates for use with auto-generating component boilerplate
    
    * Adds casing helpers to templates
    
    * Fixes Readme template
    
    * Improves guidance in fixtures files
    
    * Adds checklist for generated components, and properly escapes chart attribute in Component.test.js
    
    * Adds instructions to add your chart to index.js and webpack config to Checklist.md
    
    * Slight tweak in language
    
    * Adds instructions for Plop usage to CONTRIBUTING.md
    
    * Creates a todo for the loading state import in componentChart.js
    
    * Adds Plop configuration and templates for use with auto-generating component boilerplate
    
    * Adds casing helpers to templates
    
    * Fixes Readme template
    
    * Improves guidance in fixtures files
    
    * Adds checklist for generated components, and properly escapes chart attribute in Component.test.js
    
    * Adds instructions to add your chart to index.js and webpack config to Checklist.md
    
    * Slight tweak in language
    
    * Adds instructions for Plop usage to CONTRIBUTING.md
    
    * Creates a todo for the loading state import in componentChart.js

---
+++ plopfile.js
@@ -0,0 +1,47 @@
+module.exports = function(plop) {
+    plop.setHelper('openBrace', () => '{');
+    plop.setHelper('closeBrace', () => '}');
+    plop.setGenerator('component', {
+        description: 'Create a new component',
+        prompts: [{
+            type: 'input',
+            name: 'componentName',
+            message: 'Component name (with PascalCase capitalization, eg StackedArea)',
+        }],
+        actions: [{
+            type: 'add',
+            path: 'src/charts/{{camelCase componentName}}/{{camelCase componentName}}Chart.test.js',
+            templateFile: 'src/charts/template/componentChart.test.js',
+        },
+        {
+            type: 'add',
+            path: 'src/charts/{{camelCase componentName}}/{{camelCase componentName}}Chart.js',
+            templateFile: 'src/charts/template/componentChart.js',
+        },
+        {
+            type: 'add',
+            path: 'src/charts/{{camelCase componentName}}/{{camelCase componentName}}.fixtures.js',
+            templateFile: 'src/charts/template/componentChart.fixtures.js',
+        },
+        {
+            type: 'add',
+            path: 'src/charts/{{camelCase componentName}}/{{pascalCase componentName}}.js',
+            templateFile: 'src/charts/template/Component.js',
+        },
+        {
+            type: 'add',
+            path: 'src/charts/{{camelCase componentName}}/{{pascalCase componentName}}.test.js',
+            templateFile: 'src/charts/template/Component.test.js',
+        },
+        {
+            type: 'add',
+            path: 'src/charts/{{camelCase componentName}}/Readme.md',
+            templateFile: 'src/charts/template/Readme.md',
+        },
+        {
+            type: 'add',
+            path: 'src/charts/{{camelCase componentName}}/Checklist.md',
+            templateFile: 'src/charts/template/Checklist.md',
+        }],
+    });
+};
