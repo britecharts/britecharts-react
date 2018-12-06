b7dff95480c0cc426bbac11e112b9e0f8f699b66
David Gomez <dg.dave@gmail.com>
2018-12-06 06:14:30 -0500

    Group helpers together in the index file

--- src/charts/index.js
+++ src/charts/index.js
@@ -3,10 +3,11 @@ export {default as Donut} from './donut/Donut.js';
 export {default as GroupedBar} from './groupedBar/GroupedBar.js';
 export {default as Legend} from './legend/Legend.js';
 export {default as Line} from './line/Line.js';
-export {default as ResponsiveContainer } from './helpers/responsiveContainer.js';
 export {default as Sparkline} from './sparkline/Sparkline';
 export {default as StackedArea} from './stackedArea/StackedArea.js';
 export {default as StackedBar} from './stackedBar/StackedBar.js';
 export {default as Step} from './step/Step.js';
 export {default as Tooltip} from './tooltip/Tooltip.js';
+
+export {default as ResponsiveContainer } from './helpers/responsiveContainer.js';
 export {default as withResponsiveness} from './helpers/withResponsiveness.js';
