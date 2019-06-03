2046ba27ea279df770de5b68711cc55a929a025d
David Gomez <dg.dave@gmail.com>
2019-06-03 14:18:47 -0400

    Add StackedBar example fixtures with dates

--- src/charts/stackedBar/stackedBarChart.fixtures.js
+++ src/charts/stackedBar/stackedBarChart.fixtures.js
@@ -46,6 +46,69 @@ const with3Sources = () => [
     },
 ];
 
+const with3SourcesAndDates = () => [
+    {
+        name: '2011-01-05',
+        stack: 'vivid',
+        value: 0,
+    },
+    {
+        name: '2011-01-06',
+        stack: 'vivid',
+        value: 4,
+    },
+    {
+        name: '2011-01-07',
+        stack: 'vivid',
+        value: 12,
+    },
+    {
+        name: '2011-01-08',
+        stack: 'vivid',
+        value: 23,
+    },
+    {
+        name: '2011-01-05',
+        stack: 'sparkling',
+        value: 18,
+    },
+    {
+        name: '2011-01-06',
+        stack: 'sparkling',
+        value: 16,
+    },
+    {
+        name: '2011-01-07',
+        stack: 'sparkling',
+        value: 10,
+    },
+    {
+        name: '2011-01-08',
+        stack: 'sparkling',
+        value: 0,
+    },
+    {
+        name: '2011-01-05',
+        stack: 'sunny',
+        value: 10,
+    },
+    {
+        name: '2011-01-06',
+        stack: 'sunny',
+        value: 20,
+    },
+    {
+        name: '2011-01-07',
+        stack: 'sunny',
+        value: 16,
+    },
+    {
+        name: '2011-01-08',
+        stack: 'sunny',
+        value: 25,
+    }
+];
+
 const with2Sources = () => [
     {
         name: 'Dazzling',
@@ -81,5 +144,6 @@ const with2Sources = () => [
 
 export default {
     with3Sources,
+    with3SourcesAndDates,
     with2Sources,
 };
